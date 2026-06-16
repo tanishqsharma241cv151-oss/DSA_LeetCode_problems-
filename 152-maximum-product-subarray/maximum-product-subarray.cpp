af84f4b2e08b637dca4cee1bd3f08b65493b3543
tanishqsharma241cv151-oss <tanishqsharma.241cv151@nitk.edu.in>
2026-06-16 13:38:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (58.81%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -4,20 +4,18 @@ public:
         int maxi1=INT_MIN,maxi2=INT_MIN,n=nums.size();
         int mul1=1,mul2=1;
         for(int i=0;i<n;i++){
-            int ind=-1;
             mul1*=nums[i];
-            maxi1=max(maxi1,mul1);  
+            mul2*=nums[n-1-i];
+            maxi1=max(maxi1,mul1);
+            maxi2=max(maxi2,mul2); 
             if(mul1==0){
                 mul1=1;
             }
-        }
-        for(int i=n-1;i>=0;i--){
-            mul2*=nums[i];
-            maxi2=max(maxi2,mul2);  
             if(mul2==0){
                 mul2=1;
             }
         }
+
     return max(maxi1,maxi2);
     }
 };
