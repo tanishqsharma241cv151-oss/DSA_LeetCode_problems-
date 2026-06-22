a4ff29edebc36b6510236fb8bf7e3cde9e9dec1b
tanishqsharma241cv151-oss <tanishqsharma.241cv151@nitk.edu.in>
2026-06-22 22:39:12 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (39.05%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -11,9 +11,7 @@ public:
             if(nums[mid-1]<nums[mid]&&nums[mid]>nums[mid+1]) return mid;
             else if(nums[mid-1]<nums[mid] && nums[mid]<nums[mid+1]) low=mid+1;
             else if(nums[mid-1]>nums[mid] && nums[mid]>nums[mid+1]) high=mid-1;
-            else if(nums[mid-1]>nums[mid] && nums[mid]<nums[mid+1]){
-                low=mid+1;
-            }    
+            else{low=mid+1;}   
         }
         return -1;
     }
