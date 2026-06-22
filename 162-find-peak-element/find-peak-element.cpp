336e45f421107aed7a9a62cd4a1386ee48dec110
tanishqsharma241cv151-oss <tanishqsharma.241cv151@nitk.edu.in>
2026-06-22 22:57:29 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (39.05%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -8,9 +8,13 @@ public:
         int low=1,high=n-2;
         while(low<=high){
             int mid=low+(high-low)/2;
+            //if mid is a the peak.
             if(nums[mid-1]<nums[mid]&&nums[mid]>nums[mid+1]) return mid;
+            //if mid is on a increasing curve, peak is on the left.
             else if(nums[mid-1]<nums[mid] && nums[mid]<nums[mid+1]) low=mid+1;
+            //if mid is on a decreasing curve, peak is on the right.
             else if(nums[mid-1]>nums[mid] && nums[mid]>nums[mid+1]) high=mid-1;
+            //in case of multiple peaks,if mid is on a reverse peak we can eliminate either the left or right part doesnt matter as there are peaks on both sides.
             else{low=mid+1;}   
         }
         return -1;
