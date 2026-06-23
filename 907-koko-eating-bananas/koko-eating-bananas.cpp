51c77178efea923b0ac431a38883d088df980135
tanishqsharma241cv151-oss <tanishqsharma.241cv151@nitk.edu.in>
2026-06-24 00:44:44 +0530

    Time: 13 ms (22.61%) | Memory: 23 MB (46.32%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -10,17 +10,15 @@ public:
     int minEatingSpeed(vector<int>& piles, int h) {
         int m=*max_element(piles.begin(),piles.end());
         int low=1,high=m;
-        int ans=0;
         while(low<=high){
             int mid=low+(high-low)/2;
             if(reqtime(piles,mid)<=h){ //decrease eating speed.
-                ans=mid;
                 high=mid-1;
             }
             else{
                 low=mid+1; //increase eating speed.
             }    
         }
-        return ans;
+        return low;
     }
 };
