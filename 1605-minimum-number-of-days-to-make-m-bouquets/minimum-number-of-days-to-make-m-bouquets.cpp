13000ece53799d26093e7b733cca9c8ddf2aa022
tanishqsharma241cv151-oss <tanishqsharma.241cv151@nitk.edu.in>
2026-06-24 12:35:03 +0530

    Time: 35 ms (9.66%) | Memory: 123.4 MB (5.52%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
-    int bouquets(vector<int>bloomDay,int k,int mid){
+    bool bouquets(vector<int>bloomDay,int k,int m,int mid){
         int bouquet=0;
         int count=0;
         for(int i=0;i<bloomDay.size();i++){
             if(bloomDay[i]<=mid){
                 count++;
-                if(count==k){
-                    bouquet++;
-                    count=0;
-                }
             }
             else{
+                bouquet+=count/k;
                 count=0;
             }        
         }
-        return bouquet;
+        bouquet+=count/k;
+        return bouquet>=m;
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
         int maxi=*max_element(bloomDay.begin(),bloomDay.end());
         int mini=*min_element(bloomDay.begin(),bloomDay.end());
         int low=mini,high=maxi;
         int ans=-1;
+        if(bloomDay.size()< 1LL*m*1LL*k) return -1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(bouquets(bloomDay,k,mid)<m){
-                low=mid+1;
-            }
-            else{
-                ans=mid;
+            if(bouquets(bloomDay,k,m,mid)){
                 high=mid-1;
             }
+            else{
+                low=mid+1;
+            }
         }
-        return ans;
+        return low;
     }
 };
