1939546672e52c2f8d65b54cc33feee6ab8e5207
tanishqsharma241cv151-oss <tanishqsharma.241cv151@nitk.edu.in>
2026-06-27 23:58:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (68.26%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        //its basically doing transpose of the matrix and then reversing each row.
+        //during transpose all diagonal elements remain same, upper and lower trangle elements get interchanged.
+        int n=matrix.size(),m=matrix[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=i+1;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]); //transpose done;
+            }
+            reverse(matrix[i].begin(),matrix[i].end());//reversing the rows to get the 90 degrees rotates array.
+        }    
+    }
+};
