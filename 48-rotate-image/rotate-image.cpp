0271f3723fb1e4e98f94792f5b35e57c809206e2
tanishqsharma241cv151-oss <tanishqsharma.241cv151@nitk.edu.in>
2026-06-28 00:07:10 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (7.91%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -4,8 +4,8 @@ public:
         //its basically doing transpose of the matrix and then reversing each row.
         //during transpose all diagonal elements remain same, upper and lower trangle elements get interchanged.
         int n=matrix.size(),m=matrix[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=i+1;j<n;j++){
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<m;j++){
                 swap(matrix[i][j],matrix[j][i]); //transpose done;
             }
             reverse(matrix[i].begin(),matrix[i].end());//reversing the rows to get the 90 degrees rotates array.
