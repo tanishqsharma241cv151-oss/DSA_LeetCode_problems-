b5ced5ace783e39238c405c97bfc6144f927ff00
tanishqsharma241cv151-oss <tanishqsharma.241cv151@nitk.edu.in>
2026-06-28 18:29:18 +0530

    Time: 0 ms (100.00%) | Memory: 50.3 MB (58.24%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        //do a bs over the columns.
         int m=mat.size(),n=mat[0].size();
         int low=0,high=n-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            //take the max element in the mid column, as it could be the possible peak.
-            int maxi=INT_MIN, row=-1;
+        //take the max element in the mid col so it satisfies the criteria of element>top, element>bottom counterpart.
+            int maxi=INT_MIN,row=-1;
             for(int i=0;i<m;i++){
-                if(maxi<mat[i][mid]){
-                    maxi=mat[i][mid]; row=i;
+                if(mat[i][mid]>maxi){
+                    maxi=mat[i][mid];row=i;
                 }
             }
-            //check maxi for the 4 peak conditions, maxi > its top,bottom,left and right counterparts.
-            //since its the max element in the mid column it will already satsify maxi > top and bottom counterparts.
-            int right=(mid==n-1)?-1:mat[row][mid+1];
+        //max element in the col can potentially be a peak, to confirm we check if maxi>right,maxi>left counterpart.
             int left=(mid==0)?-1:mat[row][mid-1];
-            if(maxi>right&& maxi>left) return {row,mid};
-            else if(mat[row][mid]<right) low=mid+1;
-            else if(mat[row][mid]<left) high=mid-1;   
+            int right=(mid==n-1)?-1:mat[row][mid+1];
+            if(mat[row][mid]>left && mat[row][mid]>right) return {row,mid};
+            else if(mat[row][mid]<=left) high=mid-1;
+            else if(mat[row][mid]<=right) low=mid+1;
         }
-        return {-1,-1};
+        return {-1,-1}; //dummy statement.
 
     }
 };
