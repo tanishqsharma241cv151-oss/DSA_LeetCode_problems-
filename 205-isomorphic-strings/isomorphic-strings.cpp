1d0070f18ebe21eceff190fd64f4c132cfed23a1
tanishqsharma241cv151-oss <tanishqsharma.241cv151@nitk.edu.in>
2026-06-30 00:20:18 +0530

    Time: 4 ms (23.25%) | Memory: 9.1 MB (94.59%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int m=s.size();
+        int n=t.size();
+        if(m!=n)return false;
+        vector<int>arr1(256,-1);
+        vector<int>arr2(256,-1);
+        //to track the index of last occurence of a character
+        //the goal is to ensure that the pattern of occurence and repetition of characters is the same in both strings.
+        for(int i=0;i<n;i++){
+            if(arr1[s[i]]!=arr2[t[i]]) return false;
+            arr1[s[i]]=i;
+            arr2[t[i]]=i;     
+        }
+        return true;
+
+    }
+};
