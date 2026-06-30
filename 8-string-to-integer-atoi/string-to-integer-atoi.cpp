0fb720e59a9281180766c5a1eaf8c6c7778c98eb
tanishqsharma241cv151-oss <tanishqsharma.241cv151@nitk.edu.in>
2026-06-30 20:44:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (55.63%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int n=s.size();
+        int sign=1;
+        int num=0;
+        int i=0;
+        while(i<n){
+            if(s[i]==' '){i++;}
+            else break;
+        }
+        if(i<n && s[i]=='-'){sign*=-1;i++;}
+        else if(i<n;s[i]=='+'){sign=1;i++;}
+        while(i<n && isdigit(s[i])){
+            int dig=s[i]-'0';
+            if(num>(INT_MAX-dig)/10){
+                return sign==-1?INT_MIN:INT_MAX;
+            }
+            num=num*10+(s[i]-'0');
+            i++;  
+        }
+        return num*sign;
+    }
+};
