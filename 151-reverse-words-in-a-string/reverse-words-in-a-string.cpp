a44b20b0fc0c3fd5fde2322b91935f8633844687
tanishqsharma241cv151-oss <tanishqsharma.241cv151@nitk.edu.in>
2026-07-02 04:42:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (88.43%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -4,21 +4,17 @@ public:
         int n=s.size();
         string ans="";
         int i=n-1; 
-        while(i>=0){ //run a while loop from right to left on the input string s.
-            if(s[i]==' '){
-                i--;continue;
-            }
-            if(i<0) break;
-            int end=i;
-            while(i>=0 && s[i]!=' '){
-                i--;     
-            }
-            //append the word into ans.
-            //now add a space after every word, dont add a space if the ans substring is empty dont add space.
-            if(!ans.empty()){
-                ans+=' ';
-            }
-            ans+=s.substr(i+1,end-i);
+        int start=-1;
+        while(i>=0){
+           while(i>=0 && s[i]==' '){i--;continue;}
+           if(i<0) break;
+           int end=i;
+           while(i>=0 && isalnum(s[i])){
+                start=i;
+                i--;
+           }
+           if(!ans.empty()){ans.append(" ");}
+           ans+=s.substr(start,end-start+1);  
         }
         return ans;
     }
