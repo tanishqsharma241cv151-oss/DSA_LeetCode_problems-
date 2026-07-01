08ceda1f0ec9f64aedd098d3b1dc474c76421e8d
tanishqsharma241cv151-oss <tanishqsharma.241cv151@nitk.edu.in>
2026-07-01 20:30:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Sub Array/maximum-sub-array.cpp
+++ Difficulty: Medium/Maximum Sub Array/maximum-sub-array.cpp
@@ -2,30 +2,27 @@ class Solution {
   public:
     vector<int> findSubarray(vector<int>& arr) {
         int n=arr.size();
-        int maxstart=0,maxend=-1;
-        int start=0,end=-1;
-        int len=0,maxlen=-1;
-        int sum=0,maxsum=INT_MIN;
+        int len=0,maxlen=0;
+        int sum=0,maxsum=0;
+        int start=-1,maxstart=0,maxend=0;
         for(int i=0;i<n;i++){
             if(arr[i]>=0){
-                if(sum==0){start=i;}
+                if(sum==0) start=i;
                 sum+=arr[i];
                 len++;
-                
-                if(sum>maxsum ||(sum==maxsum && len>maxlen)){
-                    maxsum=sum;maxlen=len;maxstart=start;maxend=i;
+                if(sum>maxsum || sum==maxsum && len>maxlen ){
+                    maxstart=start;maxlen=len;maxsum=sum;maxend=i;
                 }
-                
             }
             else{
                 sum=0;len=0;
-                if(arr[i]<0) continue;
+                if(arr[i]<0)continue;
             }
-            
         }
-        if (maxend==-1) return {-1};
+        if(start==-1) return{-1};
         vector<int>ans(arr.begin()+maxstart,arr.begin()+maxend+1);
         return ans;
+        
         
     }
 };
