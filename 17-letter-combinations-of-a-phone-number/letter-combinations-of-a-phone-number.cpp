6eaf015c571a5c74573c84eb2ec8182e4e716775
tanishqsharma241cv151-oss <tanishqsharma.241cv151@nitk.edu.in>
2026-07-10 04:16:15 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (78.13%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void solve(map<int,vector<char>>&mpp,vector<string>&ans,string&digits,string curr,int ind){
+        if(ind==digits.size()){
+            ans.push_back(curr);return;}
+        for(int i=0;i<mpp[digits[ind]-'0'].size();i++){
+            curr+=mpp[digits[ind]-'0'][i];
+            solve(mpp,ans,digits,curr,ind+1);
+            curr.pop_back();
+        }
+        
+    }
+    vector<string> letterCombinations(string digits) {
+        map<int,vector<char>>mpp={
+            {2,{'a','b','c'}},{3,{'d','e','f'}},{4,{'g','h','i'}},{5,{'j','k','l'}},{6,{'m','n','o'}},{7,{'p','q','r','s'}},{8,{'t','u','v'}},{9,{'w','x','y','z'}}
+        };
+        vector<string>ans;
+        string curr="";
+        int ind=0;
+        solve(mpp,ans,digits,curr,ind);
+        return ans;
+    }
+};
