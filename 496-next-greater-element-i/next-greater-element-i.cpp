8446e3ce529bde35c3ad7102fecf243a865fa5b5
tanishqsharma241cv151-oss <tanishqsharma.241cv151@nitk.edu.in>
2026-07-14 21:18:56 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (15.71%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -7,26 +7,9 @@ public:
         unordered_map<int,int>mpp;
         vector<int>arr;
         for(int i=n2-1;i>=0;i--){
-            if(st.empty()){
-                mpp[nums2[i]]=-1;
-                st.push(nums2[i]);}
-            else{
-                if(nums2[i]<st.top()){
-                    mpp[nums2[i]]=st.top();
-                    st.push(nums2[i]);
-                }
-                else{
-                    while(!st.empty()&&nums2[i]>=st.top()){st.pop();}
-                    if(st.empty()){
-                        mpp[nums2[i]]=-1;
-                        st.push(nums2[i]);
-                    }
-                    else{
-                        mpp[nums2[i]]=st.top();
-                        st.push(nums2[i]);
-                    }
-                }
-            }
+            while(!st.empty()&&nums2[i]>=st.top()){st.pop();}
+            mpp[nums2[i]]= st.empty()?-1:st.top();
+            st.push(nums2[i]); 
         }
         for(int i=0;i<n1;i++){
             arr.push_back(mpp[nums1[i]]);
