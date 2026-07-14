0b56dcc3d6e7c784b248433f94a916349521de3f
tanishqsharma241cv151-oss <tanishqsharma.241cv151@nitk.edu.in>
2026-07-15 00:57:24 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (72.66%) - LeetSync

---
+++ 31-next-permutation/next-permutation.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0 || n==1){return;}
+        int p=-2;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                p=i;break;}
+        }
+        if(p==-2){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        int q=0;
+        for(int j=n-1;j>=0;j--){
+            if(nums[j]>nums[p]){
+                q=j;break;}
+        }
+        swap(nums[p],nums[q]);
+        sort(nums.begin()+p+1,nums.end());
+
+    }
+};
