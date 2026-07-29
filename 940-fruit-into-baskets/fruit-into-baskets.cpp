881d4f2302456678556eb15570021b20d40a7b22
tanishqsharma2006 <tanishqsharma.241cv151@nitk.edu.in>
2026-07-29 16:25:53 +0530

    Time: 51 ms (27.56%) | Memory: 85 MB (34.50%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -7,7 +7,7 @@ public:
         unordered_map<int,int>mpp;
         while(r<n){
             mpp[fruits[r]]++;
-            while(mpp.size()>2){
+            if(mpp.size()>2){
                 mpp[fruits[l]]--;
                 if(mpp[fruits[l]]==0) mpp.erase(fruits[l]);
                 l++;
