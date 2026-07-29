cba21141eb213f500dd3d56c1f9b4bb7162b69db
tanishqsharma2006 <tanishqsharma.241cv151@nitk.edu.in>
2026-07-29 16:08:21 +0530

    Time: 51 ms (27.56%) | Memory: 84.8 MB (84.39%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        //we need to find maxlength subarray with atmost 2 types of numbers.
+        int n=fruits.size();
+        int r=0,l=0,maxlen=0;
+        unordered_map<int,int>mpp;
+        while(r<n){
+            mpp[fruits[r]]++;
+            while(mpp.size()>2){
+                mpp[fruits[l]]--;
+                if(mpp[fruits[l]]==0) mpp.erase(fruits[l]);
+                l++;
+            }
+            if(mpp.size()<=2){
+                maxlen=max(maxlen,r-l+1);
+            }
+            r++;
+        }
+        return maxlen;
+    }
+};
