ebd8f1f8e85900e0d6a47119ead523c4e307f5e9
tanishqsharma2006 <tanishqsharma.241cv151@nitk.edu.in>
2026-07-31 01:00:21 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (54.32%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -4,15 +4,14 @@ public:
         if(k==1 || k==0) return 0;
         int n=nums.size();
         long long prod=1;
-        int l=0,r=0;
-        long long count=0;
+        int l=0,r=0,count=0;
         while(r<n){
             prod=(long long)prod*nums[r];
             while(prod>=k){
                 prod=(long long)prod/nums[l];l++;
             }
             if(prod<k){
-                count=(int)count+r-l+1;r++;
+                count+=r-l+1;r++;
             }
         }
         return count;
