5878a0586d4319e0d8f5719a24443bdcb1ef02c6
tanishqsharma2006 <tanishqsharma.241cv151@nitk.edu.in>
2026-07-30 16:03:31 +0530

    Time: 0 ms (100.00%) | Memory: 32.4 MB (98.69%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -4,7 +4,7 @@ public:
         //this function counts the number of subarrays with sum<=goal.
         int n=nums.size();
         int l=0,r=0,count=0,sum=0;
-        if(goal<0)return 0;
+        if(goal<0)return 0; //in case goal=0, then the 2nd function will call (nums,goal-1).
         while(r<n){
             sum+=nums[r];
             while(sum>goal){
@@ -20,6 +20,7 @@ public:
         return count;
     }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
+        //to get the count of number of subarrays with sum==goal, subtract count of subarrays with sum<=goal and count of subarrays with sum<=goal-1.
         int a=numofsubarrays(nums,goal);
         int b=numofsubarrays(nums,goal-1);
         return a-b;
