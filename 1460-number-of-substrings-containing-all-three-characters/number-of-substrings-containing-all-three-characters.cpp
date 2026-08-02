aae599b118a55e65ee308635af37c8acb9fb785d
tanishqsharma2006 <tanishqsharma.241cv151@nitk.edu.in>
2026-08-02 07:45:46 +0530

    Time: 2 ms (91.38%) | Memory: 11.1 MB (52.13%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     int numberOfSubstrings(string s) {
         int n=s.size();
-        int i=0,j=0,count=0;
-        vector<int>hash(3,0);
-        for(int j=0;j<n;j++){
-            hash[s[j]-'a']++;
-            while(hash[0]>0&&hash[1]>0&&hash[2]>0){//valid window.
-                count+=n-j;
-                hash[s[i]-'a']--;
-                i++;
+        int l=0,r=0,count=0;
+        vector<int>hash(3,-1);
+        while(r<n){
+            hash[s[r]-'a']=r;
+            if(hash[0]!=-1 && hash[1]!=-1 && hash[2]!=-1){
+                //all substrings before this substring and including this valid substring will also be valid.
+                int a=min({hash[0],hash[1],hash[2]});
+                count+=a-l+1;
             }
+            r++;
         }
         return count;
     }
